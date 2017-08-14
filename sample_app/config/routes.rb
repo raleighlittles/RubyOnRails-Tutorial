eb4418a23b6370244d5153f171436868deeed5a8
Raleigh Littles <rlittles@umail.ucsb.edu>
2017-08-14 15:10:17 -0700

    Refactored about, help, home pages and updated d default route

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get 'static_pages/about' # add in a route for the new about page
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
 end
