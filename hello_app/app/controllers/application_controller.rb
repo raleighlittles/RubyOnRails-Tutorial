c2000ec2dbbd5471830b3768ce6a991dae76abfa
Raleigh Littles <rlittles@umail.ucsb.edu>
2017-08-13 17:54:06 -0700

    Folders got messed up

--- hello_app/app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  def hello
-    # Instead of printing regular rails page, print hello world!
-    render html: "Hello world!"
-  end
-end
