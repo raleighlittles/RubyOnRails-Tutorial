6d493b8d4b6e96538e7a9ed9746f9eb0149fe785
Raleigh Littles <rlittles@umail.ucsb.edu>
2017-08-13 17:54:41 -0700

    Added back files

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'application#hello' # You need to pass the routes  so it can use hello world
-# instead of the regular rails page
-end
