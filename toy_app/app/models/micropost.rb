0d4848f1cdf525580a6d9f5b917e3280e088ebb4
Raleigh Littles <rlittles@umail.ucsb.edu>
2017-08-13 21:48:20 -0700

    Add validation to prevent empty userid/email fields

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,4 +1,6 @@
 class Micropost < ApplicationRecord
+  # make Rails infer associations between posts and users with those posts
+  belongs_to :user
   # Constrain microposts to have a maximum of 140 lines
-  validates :content, :length: {maximum: 140}
+  validates :content, :length: {maximum: 140}, presence: true
 end
