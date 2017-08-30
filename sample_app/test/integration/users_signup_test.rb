00e086959ef8bd678176e776321b2a9a345ab1e9
Raleigh Littles <rlittles@umail.ucsb.edu>
2017-08-29 18:05:56 -0700

    Added basic sign up functionality with signup page, gravatar url, and basic error catching

---
+++ sample_app/test/integration/users_signup_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    # Add capability to test error messages
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_erors'
+  end
+end
