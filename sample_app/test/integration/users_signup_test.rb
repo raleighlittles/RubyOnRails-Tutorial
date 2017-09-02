e4fce4dd635902826a2dc8b612fc95c530473e55
Raleigh Littles <rlittles@umail.ucsb.edu>
2017-09-01 18:06:00 -0700

    Finished user signup! (end of section 7.4

--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -15,4 +15,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
   end
-end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                          email: "user@example.com",
+                                          password: "password",
+                                         password_confirmation: "password" } }
+    end
+
+    follow_redirect! # render the users/show template
+    assert_template 'users/show'
+    end
+  end
