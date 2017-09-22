dc66d92db36bfe5697575deb34f77cfb151556ec
Raleigh Littles <rlittles@umail.ucsb.edu>
2017-09-21 17:57:54 -0700

    Finished section 13.3

--- sample_app/app/controllers/static_pages_controller.rb
+++ sample_app/app/controllers/static_pages_controller.rb
@@ -1,14 +1,16 @@
 class StaticPagesController < ApplicationController
 
-
   def home
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
   end
 
-  def about # add in controller for about page
+  def about
   end
 
   def contact
--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -72,6 +72,10 @@ class User < ApplicationRecord
       reset_sent_at < 2.hours.ago
     end
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
   def downcase_email
