b9b031dde9cf488d4e7f1215f3311943d447a55e
Raleigh Littles <rlittles@umail.ucsb.edu>
2017-09-21 21:52:30 -0700

    Finished section 14.1

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
 
-  # The destroy part is needed to make sure that when a user is destroyed, so are their microposts
-  has_many :microposts, dependent: :destroy
-  has_many :active_relationships, class_name:  "Relationship",
-           foreign_key: "follower_id",
-           dependent:   :destroy
+    # The destroy part is needed to make sure that when a user is destroyed, so are their microposts
+    has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name:  "Relationship",
+             foreign_key: "follower_id",
+             dependent:   :destroy
 
-  has_many :following, through: :active_relationships, source: :followed
+    has_many :passive_relationships, class_name:  "Relationship",
+             foreign_key: "followed_id",
+             dependent:   :destroy
+    has_many :following, through: :active_relationships,  source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
 
     #attr_accessor(:remember_token)
 
@@ -77,9 +81,24 @@ class User < ApplicationRecord
       reset_sent_at < 2.hours.ago
     end
 
-  def feed
-    Micropost.where("user_id = ?", id)
-  end
+    def feed
+      Micropost.where("user_id = ?", id)
+    end
+
+  #Follows a user.
+    def follow(other_user)
+      following << other_user
+    end
+
+  # Unfollows a user.
+    def unfollow(other_user)
+      following.delete(other_user)
+    end
+
+    # Returns true if the current user is following the other user.
+    def following?(other_user)
+      following.include?(other_user)
+    end
 
   private
 
--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -85,7 +85,21 @@ test "associated microposts should be destroyed if a user is destroyed" do
   assert_difference 'Micropost.count', -1 do
   @user.destroy
   end
+end
+
+
+  test "should follow and unfollow a user" do
+    michael  = users(:michael)
+    archer   = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
   end
+
+
 
 
 end # end class
